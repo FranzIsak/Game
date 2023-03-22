d143ec10e9ca51f1e028ea6d0a61195bd0e8bb39
FranzIsak <franzisak99@gmail.com>
2023-03-22 15:42:25 +0000

    Adding background layer foundation

--- library/background.js
+++ library/background.js
@@ -1,3 +1,30 @@
-class Layer {
-    
+export class Layer {
+    constructor(game){//, width, height, speedModifier, image){
+        this.game = game;
+        this.canvasWidth = this.game.width;
+        this.canvasHeight = this.game.height;
+        // Player information
+        this.player = this.game.player;
+        this.playerX = this.player.x;
+        this.playerY = this.player.y;
+        this.playerWidth = this.player.width;
+        this.playerHeight = this.player.height;
+        
+        // Start Location of background image
+        this.x = this.pl;
+        this.y = 0;
+
+        this.mainBackground = MainBackground;
+        
+    }
+    update(){
+        this.playerX = -this.player.x;
+        this.playerY = -this.player.y;
+        console.log(this.playerX);
+        console.log(this.playerY);
+    }
+    draw(context){
+        this.context = context;
+        this.context.drawImage(this.mainBackground, this.playerX, -250);
+    }
 }
